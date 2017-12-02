80303517f134d0486f03f0adc5769af75e13c239
BenoitGoutorbe <benoit.goutorbe@gmail.com>
2017-12-02 17:49:03 +0100

    Reading the genes sets data file

--- gsea.py
+++ gsea.py
@@ -16,4 +16,20 @@ for line in content[1:] :
 
 print("Expression levels of", len(genes), "genes for",len(patients), "patients")
 data = np.matrix(expr)
-print(data.shape)
+
+# reading the patwhays file
+file = open("pathways.txt", "r")
+content = file.readlines()
+file.close()
+
+MIN_SIZE = 15
+pathways = [] # name of the pathways
+genes_sets = [] # name of the genes implicated in each pathway
+# NOTE : we keep only the genes sets with at least MIN_SIZE (15) genes for which we have expression data
+for line in content :
+    genes_implicated = [g for g in line.split()[1:] if g in genes]
+    if len(genes_implicated) >= MIN_SIZE :
+        pathways.append(line.split()[0])
+        genes_sets.append(genes_implicated)
+
+print(len(pathways),"sets of genes used of the analysis")
