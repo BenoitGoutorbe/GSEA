ec1aa4f0907ca55c27c757eba82c5670d4ecd781
BenoitGoutorbe <benoit.goutorbe@gmail.com>
2017-12-07 21:05:10 +0100

    Few modifications
    Can be used from terminal now
    Can be used for other category than 'AML' and 'ALL'

--- gsea.py
+++ gsea.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import decimal
+import sys
 
 class gsea:
     def __init__(self, expression_file, pathway_file, output_file,MIN_SIZE = 2):
@@ -14,6 +14,8 @@ class gsea:
         self.genes = []  # name of the genes (headers)
         expr = []  # table with expression levels
         self.patients = content[0].split()[1:]
+        self.categories = list(set(self.patients))
+        if (len(self.categories)!= 2) : print("2 categories of patients needed. given :",self.categories)
         for line in content[1:]:
             self.genes.append(line.split()[0])
             expr.append([int(value) for value in line.split()[1:]])
@@ -22,7 +24,7 @@ class gsea:
         file = open(pathway_file, "r")
         content = file.readlines()
         file.close()
-        print("Input data :" , len(self.patients) , "patients," ,
+        print("Input data :" , len(self.patients) , "patients",self.categories,"," ,
               len(self.genes) , "genes, " , len(content) , "pathways")
 
         self.pathways = []  # name of the pathways
@@ -47,27 +49,27 @@ class gsea:
         self.expr_matrix = np.matrix(expr)
 
         self.index_genes_implicated = []
-        for set in self.genes_sets :
-            self.index_genes_implicated.append([self.genes.index(g) for g in set])
+        for set_i in self.genes_sets :
+            self.index_genes_implicated.append([self.genes.index(g) for g in set_i])
 
         self.NB_patients = len(self.patients)
         self.NB_genes = len(self.genes)
         self.NB_sets = len(self.pathways)
         print("Reduced data (only pathways with at least",MIN_SIZE,"genes and only genes belonging to a selected pathway)")
-        print(self.NB_patients , "patients," , self.NB_genes , "genes, " , self.NB_sets , "pathways")
+        print(self.NB_genes , "genes, " , self.NB_sets , "pathways")
 
     def get_ES(self, patients = [], show = False,p = 1):
         if len(patients) == 0 :
             patients = self.patients
 
-        ALL_index = [i for i in range(self.NB_patients) if patients[i] == 'ALL']
-        AML_index = [i for i in range(self.NB_patients) if patients[i] == 'AML']
+        cat1_index = [i for i in range(self.NB_patients) if patients[i] == self.categories[0]]
+        cat2_index = [i for i in range(self.NB_patients) if patients[i] == self.categories[1]]
         dif_expr = []
 
         for i in range(self.NB_genes):
-            expr_ALL = np.mean([self.expr_matrix[i,j] for j in ALL_index])
-            expr_AML = np.mean([self.expr_matrix[i,j] for j in AML_index])
-            dif_expr.append(expr_ALL-expr_AML) # negative values indicate a gene expressed (in average) more by AML patients
+            expr_cat1 = np.mean([self.expr_matrix[i,j] for j in cat1_index])
+            expr_cat2 = np.mean([self.expr_matrix[i,j] for j in cat2_index])
+            dif_expr.append(expr_cat1-expr_cat2) # negative values indicate a gene expressed (in average) more by category 2
 
         self.correlation = list(dif_expr)
         # sorting the genes by correlation with phenotye
@@ -85,7 +87,8 @@ class gsea:
                     dist2_0.append(dist2_0[-1] + pow(abs(dif_expr[i]), p) / N_R)
                 else:
                     dist2_0.append(dist2_0[-1] - 1.0/(self.NB_genes-N_H) )
-            ES.append(np.max(np.abs(dist2_0)))
+            max_deviation_position = np.argmax(np.abs(dist2_0))
+            ES.append(dist2_0[max_deviation_position])
         if show :
             plt.subplot(121)
             plt.plot(dif_expr)
@@ -98,18 +101,15 @@ class gsea:
 
     def get_ES_fast(self, patients = [], show = False,p = 1):
         if len(patients) == 0 :
-            remind = True
             patients = self.patients
-
-        ALL_index = [i for i in range(self.NB_patients) if patients[i] == 'ALL']
-        AML_index = [i for i in range(self.NB_patients) if patients[i] == 'AML']
+        cat1_index = [i for i in range(self.NB_patients) if patients[i] == self.categories[0]]
+        cat2_index = [i for i in range(self.NB_patients) if patients[i] == self.categories[1]]
         dif_expr = []
 
         for i in range(self.NB_genes):
-            expr_ALL = np.mean([self.expr_matrix[i,j] for j in ALL_index])
-            expr_AML = np.mean([self.expr_matrix[i,j] for j in AML_index])
-            dif_expr.append(expr_ALL-expr_AML) # negative values indicate a gene expressed (in average) more by AML patients
-
+            expr_cat1 = np.mean([self.expr_matrix[i, j] for j in cat1_index])
+            expr_cat2 = np.mean([self.expr_matrix[i, j] for j in cat2_index])
+            dif_expr.append(expr_cat1 - expr_cat2)  # negative values indicate a gene expressed (in average) more by cat1
         self.correlation = list(dif_expr)
         # sorting the genes by correlation with phenotye
         index_sort = np.argsort(dif_expr).tolist()
@@ -122,20 +122,18 @@ class gsea:
             N_R = np.sum(pow(abs(dif_expr[i]),p) for i in self.index_genes_implicated[set])
             penalty = -1.0/(self.NB_genes-N_H)
             positions_genes_implicated = np.sort([positions[i] for i in self.index_genes_implicated[set]]).tolist()
-            path = [0]
+            path = []
             if positions_genes_implicated[0] != 0 :
-                path.append((positions_genes_implicated[0])* penalty)
+                path.append(positions_genes_implicated[0]* penalty)
                 path.append(path[-1] + pow(abs(dif_expr_sorted[positions_genes_implicated[0]]), p) / N_R)
             else :
-                path = [pow(abs(dif_expr[positions_genes_implicated[0]]), p) /N_R]
+                path .append(pow(abs(dif_expr[positions_genes_implicated[0]]), p) /N_R)
             for i in range(1,len(positions_genes_implicated)) :
                 path.append(path[-1]+(positions_genes_implicated[i]-positions_genes_implicated[i-1]-1)*penalty)
                 path.append(path[-1] + pow(abs(dif_expr_sorted[positions_genes_implicated[i]]), p) /N_R)
-            path.append(path[-1]+float((self.NB_genes-positions_genes_implicated[-1]-1)*penalty))
+            path.append(path[-1]+(self.NB_genes-positions_genes_implicated[-1]-1)*penalty)
             max_deviation_position = np.argmax(np.abs(path))
             ES.append(path[max_deviation_position])
-
-
         if show :
             plt.subplot(121)
             plt.plot(np.sort(dif_expr))
@@ -203,36 +201,49 @@ class gsea:
                     NES.append(ES[i] / ES_neg_means[i])
         else :
             NES = ES
+        if normalize: print('  DONE')
+        print("Computing p-values ...", end = '', flush=True)
         pval = []
         for i in range(self.NB_sets) :
             if NES[i] > 0 :
                 pval.append(np.sum(np.greater(random_distrib,NES[i])) / len(random_distrib))
             else :
                 pval.append(np.sum(np.less(random_distrib,NES[i])) / len(random_distrib))
-        if normalize: print('  DONE')
+        print('  DONE')
         if show :
+            print('Creating the histogram ...', end = '', flush=True)
             xmin, xmax = np.min(random_distrib)-0.2, np.max(random_distrib)+0.2
             x = np.arange(xmin,xmax,0.1).tolist()
-            y = [ sum([val >= xi and val < xi+0.1 for val in random_distrib]) for xi in x]
+            y = [ sum([val >= xi and val < xi+0.1 for val in random_distrib]) for xi in x] #really slow
             y = (np.array(y) / size_sample).tolist()
-            plt.plot(np.array(x)+0.05,y, 'r--')
-            plt.title("Random distribution scores for " + str(size_sample*self.NB_sets) + " sets of genes")
-            plt.hist(NES, bins=x)
-            plt.savefig('distribution.png')
+            plt.plot(np.array(x)+0.05,y, 'r--', label = 'Null distribution')
+            plt.hist(NES, bins=x, label= 'Observed NES')
+            plt.xlabel('Normalized Enrichment Scores')
+            plt.ylabel('Number of sets')
+            plt.legend()
+            plt.savefig(self.output_file+".png")
+            print('  DONE')
         return (pval, NES)
 
-    def write_output(self, p_values,  norm_scores, alpha = 0.05):
-        file = open(self.output_file, 'w')
-        file.write("Pathway p-value NES Leukemia Correlation \n")
-        nb_hits = max(10, len(np.argwhere(np.array(p_values) < alpha)))
-        hits = np.argsort(p_values).tolist()[:nb_hits]
+    def write_output(self, p_values, norm_scores):
+        file = open(self.output_file+".txt", 'w')
+        file.write("p-value\tNES\tOverExpressingCategory\tPathway")
+        hits = np.argsort(p_values).tolist()
         for set in hits :
             mean_correlation = np.mean([self.correlation[g] for g in self.index_genes_implicated[set]])
             if mean_correlation < 0 :
-                leu = "\tAML\t"
+                category = self.categories[1]
             else :
-                leu = "\tALL\t"
-            print(self.pathways[set]+"\t"+str(p_values[set])+"\t"+str(norm_scores[set])+leu+ str(mean_correlation))
-            file.write(self.pathways[set]+"\t"+str(p_values[set])+"\t"+str(norm_scores[set])+leu+ str(mean_correlation)+" \n")
+                category = self.categories[0]
+            file.write(str(p_values[set])+"\t"+str(norm_scores[set])+"\t"+category+"\t"+self.pathways[set]+"\n")
         file.close()
-        if p_values[hits[0]] >= alpha: print('NO SETS WITH p-value < ', alpha, '  ->  WE SAVED THE 10 BEST HITS')
+
+
+if len(sys.argv) == 4 :
+    expr = sys.argv[1]
+    sets = sys.argv[2]
+    out = sys.argv[3]
+    my_gsea = gsea(expr, sets, out, MIN_SIZE=15)
+    ES = my_gsea.get_ES_fast([], False, 1.0)
+    pval, norm_scores = my_gsea.get_pvalue(ES, size_sample=1000, p=1.0, normalize=True, show=True)
+    my_gsea.write_output(pval, norm_scores)
--- start_gsea.py
+++ start_gsea.py
@@ -2,22 +2,20 @@ from gsea import *
 import time
 
 
-my_gsea = gsea("leukemia.txt", "pathways.txt", "output.txt", MIN_SIZE = 2)
-"""tstart =time.time()
+my_gsea = gsea("leukemia.txt", "pathways.txt", "output", MIN_SIZE = 20)
+tstart =time.time()
 ES1 = my_gsea.get_ES([],False, 1.0)
 t1 =time.time()-tstart
-
 tstart =time.time()
 ES2 = my_gsea.get_ES_fast([], False, 1.0)
 t2 =time.time()-tstart
 
 print("comparaison des 2 methodes :" )
+print("scores      (",round(t1,3),"s )")
+print("scores fast (",round(t2,3),"s )")
 
-print("scores      (",round(t1,3),"s ) :",ES1[:10])
-print("scores fast (",round(t2,3),"s ) :",ES2[:10])
-"""
 ES  = my_gsea.get_ES_fast([], False, 1.0)
-pval, norm_scores = my_gsea.get_pvalue(ES,size_sample=1000, p = 1.0, normalize=True, show=True)
-my_gsea.write_output(pval, norm_scores, alpha = 0.05)
+pval, norm_scores = my_gsea.get_pvalue(ES,size_sample=500, p = 1.0, normalize=True, show=True)
+my_gsea.write_output(pval, norm_scores)
 
 
